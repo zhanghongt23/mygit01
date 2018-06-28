7d7ee1f1a22074776c52b04c0d5db798c1f77f20
zhanghongt <997308965@qq.com>
2018-06-28 15:08:46 +0800

    修复用户名的bug

--- readme.h
+++ readme.h
@@ -3,7 +3,7 @@ Date: 2018-06-26
 
 Git is a version control system.
 Github is great.
-I have an account in Github: zhanghongt23.
+I have an account in Github: zhanghongt.
 Create a new branch is quick and simple.
 You can forbid fast forward mode.
 
